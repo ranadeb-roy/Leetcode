ab47bf05bedba2ff14bddece577abd43bd01c5f9
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-09-14 19:37:03 +0530

    Pattern21 updated

--- basics/patterns/Pattern21.java
+++ basics/patterns/Pattern21.java
@@ -15,16 +15,14 @@ public class Pattern21 {
 
         for (int i = 1; i <= n; i++) {
             
-            if (i == 1 || i == n) {
-                for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || j == 1 || i == n || j == n) {
                     System.out.print("* ");
-                }
-            } else {
-                System.out.print("* ");
-                for (int k = 1; k <= n - 2; k++) {
+                } else {
                     System.out.print("  ");
                 }
-                System.out.print("* ");
+
             }
 
             System.out.println();
